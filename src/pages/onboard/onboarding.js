b8e06d83cad6b5d3b7a29b3004bf26753442dbf0
prinxard <prinxard@gmail.com>
2021-09-06 10:45:36 +0100

    update

--- src/pages/onboard/onboarding.js
+++ src/pages/onboard/onboarding.js
@@ -26,8 +26,8 @@ const Index = () => (
                     <Submitonboard  />
                     <p>After which a token is sent via email/SMS to the registered email and phone number linked to the Tax Identification Number.</p>
                     <p className="mt-5 flex"><span><FiAlertTriangle color={'#ffd700'} size={'2rem'} /></span> <strong> User is required to have a valid Tax Identification Number to be able to sign up on the portal.</strong></p>
-                    <p className="mt-5"><strong>Note:</strong> To register for a Tax Identification Number, user can visit the State IRS website (self-service) or click on Get KGTIN of the registration menu on the signup page on the portal [INDIVIDUAL] or [BUSINESS] as it applies to you.</p>
-                    <p className="mt-4">After clicking on submit, a window like this will appear and you will have to fill in all the necessary details; create a password and enter the token sent to your phone/email). Once done click the “submit” button.</p>
+                    <p className="mt-5"><strong>Note:</strong> To register for a Tax Identification Number, user can visit the State IRS website (self-service)  or click on Get <br /> KGTIN of the registration menu on the signup page on the portal [INDIVIDUAL] or [BUSINESS] as it applies to you.</p>
+                    <p className="mt-4">After clicking on submit, a window like this will appear and you will have to fill in all the necessary details; <br /> create a password and enter the token sent to your phone/email). Once done click the “submit” button.</p>
                 </div>
                 <div className="my-8">
                     <Signinguppage />
--- src/pages/payment/new-payment.js
+++ src/pages/payment/new-payment.js
@@ -25,7 +25,7 @@ const Index = () => (
                        4. Enter the Amount you wish to pay
                     </li>
                     <li>
-                      5.  Click on the “Select Payment Channel” which will drop down all the available payment channels (Interswitch, Bank, Remita and eTransact).
+                      5.  Click on the “Select Payment Channel” which will drop down all the available <br /> payment channels (Interswitch, Bank, Remita and eTransact).
                     </li>   
                     <li>
                       6.  After all these is done, click on “Make Payment”
