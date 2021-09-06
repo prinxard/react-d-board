0284c937450eeb4eb18a58e789120973d7fd8d6f
prinxard <prinxard@gmail.com>
2021-09-06 10:16:34 +0100

    update

--- src/pages/payment/new-payment.js
+++ src/pages/payment/new-payment.js
@@ -28,7 +28,7 @@ const Index = () => (
                       5.  Click on the “Select Payment Channel” which will drop down all the available payment channels (Interswitch, Bank, Remita and eTransact).
                     </li>   
                     <li>
-                      6.  After all these is done, click on “Make Payment”(6)
+                      6.  After all these is done, click on “Make Payment”
                     </li>
                 </ul>
                 <div className="my-7">
